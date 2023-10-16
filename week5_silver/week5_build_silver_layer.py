dc9a8af6dad583ad78e542f2ce0a9716a0e5fb5f
Scout Stalcup <scouts@propper.com>
2023-10-15 19:34:40 -0500

    Start of week 5 hw

--- week5_silver/week5_build_silver_layer.py
+++ week5_silver/week5_build_silver_layer.py
@@ -2,7 +2,7 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import desc
 from pyspark.sql.functions import current_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, DateType
 
 # Load environment variables.
 from dotenv import load_dotenv
@@ -11,6 +11,7 @@ load_dotenv()
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
+kafka_topic = "reviews"
 
 # Create a SparkSession
 spark = SparkSession.builder \
@@ -29,15 +30,84 @@ logger = spark.sparkContext._jvm.org.apache.log4j
 logger.LogManager.getLogger("org.apache.spark.util.ShutdownHookManager"). setLevel( logger.Level.OFF )
 logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level.ERROR )
 
-bronze_schema = None
+bronze_schema = StructType(
+    StructField("marketplace", StringType, nullable=False)
+    ,StructField("customer_id", StringType, nullable=False)
+    ,StructField("review_id", StringType, nullable=False)
+    ,StructField("product_id", StringType, nullable=False)
+    ,StructField("product_parent", StringType, nullable=False)
+    ,StructField("product_title", StringType, nullable=False)
+    ,StructField("product_category", StringType, nullable=False)
+    ,StructField("star_rating", IntegerType, nullable=False)
+    ,StructField("helpful_votes", IntegerType, nullable=False)
+    ,StructField("total_votes", IntegerType, nullable=False)
+    ,StructField("vine", StringType, nullable=False)
+    ,StructField("verified_purchase", StringType, nullable=False)
+    ,StructField("review_headline", StringType, nullable=False)
+    ,StructField("review_body", StringType, nullable=False)
+    ,StructField("purchase_date", DateType, nullable=False)
+    ,StructField("review_timestamp", TimestampType, nullable=False)
+)
 
-bronze_reviews = None
+# Define a streaming dataframe using readStream on top of the bronze reviews directory on S3
+bronze_reviews = spark \
+                .readStream \
+                .schema(bronze_schema) \
+                .format("parquet") \
+                .option("startingOffsets", "earliest") \
+                .option("maxOffsetsPerTrigger", "1000") \
+                .option("subscribe", kafka_topic) \
+                .option("s3a://hwe-fall-2023/sstalcup/bronze/reviews") \
+                .load
 
-bronze_customers = None
+# Register a virtual view on top of that dataframe
+bronze_reviews.createOrReplaceTempView("reviews")
 
-silver_data = None
+# Define a non-streaming dataframe using read on top of the bronze customers directory on S3
+bronze_customers = spark.read.parquet("s3a://hwe-fall-2023/sstalcup/bronze/customers")
 
-streaming_query = None
+# Register a virtual view on top of that dataframe
+bronze_customers.createOrReplaceTempView("customers")
+
+# Silver dataframe
+silver_data = spark.sql("SELECT \
+                            r.marketplace, \
+                            r.customer_id, \
+                            r.review_id, \
+                            r.product_id, \
+                            r.product_parent, \
+                            r.product_title, \
+                            r.product_category, \
+                            r.star_rating, \
+                            r.helpful_votes, \
+                            r.total_votes, \
+                            r.vine, \
+                            r.verified_purchase, \
+                            r.review_headline, \
+                            r.review_body, \
+                            r.purchase_date, \
+                            r.review_timestamp, \
+                            c.customer_id, \
+                            c.customer_name, \
+                            c.gender, \
+                            c.date_of_birth, \
+                            c.city, \
+                            c.state \
+                        FROM \
+                            reviews r \
+                        INNER JOIN \
+                            customers c \
+                        ON \
+                            r.customer_id = c.customer_id AND \
+                            c.verified_purchase = 'Y'")
+
+
+streaming_query = silver_data \
+                .writeStream \
+                .format("parquet") \
+                .option("path", "s3a://hwe-fall-2023/sstalcup/silver/reviews") \
+                .option("checkpointLocation", "/tmp/kafka-checkpoint") \
+                .start()
 
 streaming_query.start().awaitTermination()
 
